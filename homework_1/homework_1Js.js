e3dc98b029c70be458a771b03171dfacd4c03c69
Your Name <Hubrisy>
2021-09-24 13:56:43 +0300

    i fixed my faults

--- homework_1/homework_1Js.js
+++ homework_1/homework_1Js.js
@@ -1,45 +1,45 @@
-let phone = 15.678;
-let car =  123.965;
-let computer = 90.2345;
+const phone = 15.678;
+const car =  123.965;
+const computer = 90.2345;
 
-let maxSum = Math.max(phone,car,computer);
+const maxSum = Math.max(phone,car,computer);
 console.log(maxSum);
-let minSum = Math.min(phone,car,computer);
+const minSum = Math.min(phone,car,computer);
 console.log(minSum);
 
-let sumOfProducts = +(phone+car+computer);
+const sumOfProducts = (phone+car+computer);
 console.log(sumOfProducts);
 
-let floorSum = +(Math.floor(phone+car+computer));
+let floorSum = Math.floor(phone) + Math.floor(car)+Math.floor(computer);
 console.log(floorSum);
 
 let roundOfSum = (Math.round(sumOfProducts/100))*100;
 console.log(roundOfSum);
 
-let parityOfNum = roundOfSum % 2 == 0;
-console.log(parityOfNum);
+let isOdd = floorSum % 2 == 0;
+console.log(isOdd);
 
-let balance = 500 - sumOfProducts;
+const balance = 500 - sumOfProducts;
 console.log(balance);
 
 let averageSum = (sumOfProducts/3);
 console.log(averageSum.toFixed(2));
 
-let sale = Math.random()*101;
-console.log(sale);
-let sumWithSale = (sumOfProducts)-(((sumOfProducts)*sale)/100);
-console.log(sumWithSale.toFixed(2));
-let profit = (sumOfProducts/2)-(((sumOfProducts)*sale)/100);
+let discount = Math.random()*100;
+console.log(discount);
+let sumWithDiscount = (sumOfProducts)-(((sumOfProducts)*discount)/100);
+console.log(sumWithDiscount.toFixed(2));
+let profit = (sumOfProducts/2)-(((sumOfProducts)*discount)/100);
 console.log(profit);
 
-let maxSumId = document.getElementById("maxSum").innerHTML = "Максимальное число:" + maxSum;
-let minSumId =  document.getElementById("minSum").innerHTML = "Минимальное  число:" + minSum;
-let sumOfProductsId =  document.getElementById("sumOfProducts").innerHTML = "Сумма всех товаров:" + sumOfProducts;
-let floorSumId =  document.getElementById("floorSum").innerHTML = "Сумма товаров округленная в меньшую сторону:" + floorSum;
-let roundOfSumId =  document.getElementById("roundSum").innerHTML = "Округление к сотням:" + roundOfSum;
-let parityOfNumId =  document.getElementById("boolean").innerHTML = "Boolean:" + parityOfNum;
-let balanceId =  document.getElementById("delievery").innerHTML = "Сдача с 500грн:" + balance;
-let averageSumId =  document.getElementById("average").innerHTML = "Среднее значение цен:" + averageSum.toFixed(2);
-let saleId =  document.getElementById("randomDiscount").innerHTML = "Случайная скидка(%):" + sale.toFixed(2);
-let sumWithSaleId =  document.getElementById("priceWithDiscount").innerHTML = "Сумма со скидкой:" + sumWithSale.toFixed(2);
-let profitId =  document.getElementById("profit").innerHTML = "Чистая прибыль:" + profit.toFixed(2);
+document.writeln(`Максимальное число: = ${maxSum}<br>
+Минимальное число = ${minSum}<br>
+Сумма всех товаров = ${sumOfProducts}<br>
+Сумма товаров округленная в меньшую сторону = ${floorSum}<br>
+Округление к сотням = ${roundOfSum}<br>
+Boolean = ${isOdd}<br>
+Сдача с 500грн = ${balance}<br>
+Среднее значение цен = ${averageSum.toFixed(2)}<br>
+Случайная скидка(%) = ${discount.toFixed(2)}<br>
+Сумма со скидкой = ${sumWithDiscount.toFixed(2)}<br>
+Чистая прибыль = ${profit.toFixed(2)}<br>`)
