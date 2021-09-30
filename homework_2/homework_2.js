8ce3d0dd2a7e5b89bf0c0afaf935c86f269e965d
Your Name <Hubrisy>
2021-09-30 12:03:39 +0300

    new changes

--- homework_2/homework_2.js
+++ homework_2/homework_2.js
@@ -2,12 +2,12 @@ let N;
 let M;
 do{
     N = +prompt("choose your first INTEGER number", 0);
-}while(Number.isInteger(N) === false);
+}while(!Number.isInteger(N));
 console.log(N);
 
 do{
     M = +prompt("choose your second INTEGER number", 0);
-}while(Number.isInteger(M) === false);
+}while(!Number.isInteger(M));
 console.log(M);
 
 const takePairedNum = confirm('should i skip paired numbers?');
