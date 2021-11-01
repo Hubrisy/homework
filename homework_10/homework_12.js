efa9b21d05960ac626d4adcc866b963afb832f90
Your Name <Hubrisy>
2021-11-01 19:13:31 +0200

    new homework

---
+++ homework_10/homework_12.js
@@ -0,0 +1,47 @@
+    document.getElementById("DO").addEventListener('click', function() {
+      let myaudio = document.getElementById("sound1");
+      myaudio.play();
+    }
+    );
+   
+    document.getElementById("RE").addEventListener('click', function() {
+        let myaudio = document.getElementById("sound2");
+        myaudio.play();
+      }
+      );
+
+    document.getElementById("MI").addEventListener('click', function() {
+        let myaudio = document.getElementById("sound3");
+        myaudio.play();
+      }
+      );
+
+    document.getElementById("FA").addEventListener('click', function() {
+        let myaudio = document.getElementById("sound4");
+        myaudio.play();
+      }
+      );
+
+    document.getElementById("SOL").addEventListener('click', function() {
+        let myaudio = document.getElementById("sound5");
+        myaudio.play();
+      }
+      );
+
+    document.getElementById("LA").addEventListener('click', function() {
+        let myaudio = document.getElementById("sound6");
+        myaudio.play();
+      }
+      );
+
+    document.getElementById("SI").addEventListener('click', function() {
+        let myaudio = document.getElementById("sound7");
+        myaudio.play();
+      }
+      );
+
+    document.getElementById("highDO").addEventListener('click', function() {
+        let myaudio = document.getElementById("sound8");
+        myaudio.play();
+      }
+      );
