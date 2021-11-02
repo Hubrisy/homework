0782846a21112f0c760ae32a8af5b266b3858e7f
Your Name <Hubrisy>
2021-11-02 14:17:58 +0200

    new changes

--- homework_10/homework_12.js
+++ homework_10/homework_12.js
@@ -1,47 +1,8 @@
-    document.getElementById("DO").addEventListener('click', function() {
-      let myaudio = document.getElementById("sound1");
-      myaudio.play();
-    }
-    );
-   
-    document.getElementById("RE").addEventListener('click', function() {
-        let myaudio = document.getElementById("sound2");
-        myaudio.play();
-      }
-      );
-
-    document.getElementById("MI").addEventListener('click', function() {
-        let myaudio = document.getElementById("sound3");
-        myaudio.play();
-      }
-      );
-
-    document.getElementById("FA").addEventListener('click', function() {
-        let myaudio = document.getElementById("sound4");
-        myaudio.play();
-      }
-      );
-
-    document.getElementById("SOL").addEventListener('click', function() {
-        let myaudio = document.getElementById("sound5");
-        myaudio.play();
-      }
-      );
-
-    document.getElementById("LA").addEventListener('click', function() {
-        let myaudio = document.getElementById("sound6");
-        myaudio.play();
-      }
-      );
-
-    document.getElementById("SI").addEventListener('click', function() {
-        let myaudio = document.getElementById("sound7");
-        myaudio.play();
-      }
-      );
-
-    document.getElementById("highDO").addEventListener('click', function() {
-        let myaudio = document.getElementById("sound8");
-        myaudio.play();
-      }
-      );
+window.addEventListener('keydown', function Play(e){
+  let audio = document.querySelector(`audio[data-key = '${e.keyCode}']`);
+  if(!audio){
+    return
+  }
+  audio.currentTime = 0;
+  audio.play();
+})
